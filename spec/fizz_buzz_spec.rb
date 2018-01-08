64198611ef4b954157a301e589554ada5d83b9e2
Oskar Brännström <oskar@quad.se>
2018-01-08 12:51:48 +0100

    Add number spec

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,11 +1,9 @@
-require '.lib/fizz_buzz'
+require './lib/fizz_buzz'
 
 describe 'fizz_buzz' do
 
-end
-
- #spec/fizz_buzz_spec.rb
- 
-it 'returns 1 if number is 1' do
+    it 'returns 1 if number is 1' do
         expect(fizz_buzz(1)).to eq 1
+    end
+
 end
