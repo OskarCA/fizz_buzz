d86086461e0f8a349942d90392a3adcd2300ed22
Oskar Brännström <oskar@quad.se>
2018-01-08 15:27:18 +0100

    Added % 3 == 0, % 5 == 0, fizz & buzz

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,4 +1,9 @@
-#lib/fizz_buzz.rb
-    def fizz_buzz(number)
+def fizz_buzz(number)
+    if number % 3 == 0
+        'fizz'
+    elsif number % 5 == 0
+        'buzz'
+    else
         number
     end
+end
